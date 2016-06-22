c7473e106fb76ec2be311eb0b514baa90ab814d3
Xavier Bauquet <xavier.bauquet@mind-and-go.com>
2016-06-22 10:35:14 +0200

    ADD: search bar for github user name

--- app/src/main/java/com/xavier/spikedroid/githubrepositories/RepositoryListActivity.java
+++ app/src/main/java/com/xavier/spikedroid/githubrepositories/RepositoryListActivity.java
@@ -8,9 +8,14 @@ import android.os.IBinder;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.util.Log;
+import android.view.KeyEvent;
 import android.view.View;
+import android.view.inputmethod.EditorInfo;
 import android.widget.AdapterView;
+import android.widget.EditText;
 import android.widget.ListView;
+import android.widget.TextView;
+import android.widget.Toast;
 
 import com.xavier.spikedroid.R;
 
@@ -18,11 +23,18 @@ public class RepositoryListActivity extends AppCompatActivity {
 
     private boolean bounded = false;
     private RepositoryListAdapter adapter;
+    private String userName = "xbauquet";
+    private GitHubService.GitHubServiceBinder binder;
+    private TextView userNameTextView;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_repository_list);
+
+
+        userNameTextView = (TextView) findViewById(R.id.github_user_name);
+        userNameTextView.setText(userName + " repositories :");
 
         adapter = new RepositoryListAdapter(this);
         ListView list = (ListView) findViewById(R.id.repository_list);
@@ -34,11 +46,21 @@ public class RepositoryListActivity extends AppCompatActivity {
                 Intent intent = new Intent(RepositoryListActivity.this, RepositoryDetailActivity.class);
                 intent.putExtra("repository", repository);
                 startActivity(intent);
-
-
             }
         });
 
+        final EditText searchRepositories = (EditText) findViewById(R.id.search_repositories);
+        assert searchRepositories != null;
+        searchRepositories.setOnEditorActionListener(new TextView.OnEditorActionListener() {
+            public boolean onEditorAction(TextView v, int actionId, KeyEvent event) {
+                if ((event != null && (event.getKeyCode() == KeyEvent.KEYCODE_ENTER)) || (actionId == EditorInfo.IME_ACTION_DONE)) {
+                    binder.getService().getRepositories(searchRepositories.getText().toString(), adapter);
+                    userNameTextView.setText(searchRepositories.getText().toString() + " repositories :");
+                    Toast.makeText(RepositoryListActivity.this, "Loading ...", Toast.LENGTH_SHORT).show();
+                }
+                return false;
+            }
+        });
     }
 
     // ========================================
@@ -73,10 +95,8 @@ public class RepositoryListActivity extends AppCompatActivity {
         @Override
         public void onServiceConnected(ComponentName className,
                                        IBinder service) {
-            Log.e("repo", "repo");
-            GitHubService.GitHubServiceBinder binder = (GitHubService.GitHubServiceBinder) service;
-            binder.getService().getRepositories("xbauquet", adapter);
-            //MainActivity.this.unbindService(this);
+            binder = (GitHubService.GitHubServiceBinder) service;
+            binder.getService().getRepositories(userName, adapter);
         }
 
         @Override
