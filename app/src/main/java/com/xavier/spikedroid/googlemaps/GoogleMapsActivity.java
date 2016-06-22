7810b3b97effa66dd12c970023ad79eee27d5b70
Xavier Bauquet <xavier.bauquet@mind-and-go.com>
2016-06-22 11:17:53 +0200

    ADD: add toggle button to change map type + Fab

--- app/src/main/java/com/xavier/spikedroid/googlemaps/GoogleMapsActivity.java
+++ app/src/main/java/com/xavier/spikedroid/googlemaps/GoogleMapsActivity.java
@@ -11,6 +11,12 @@ import com.google.android.gms.maps.model.LatLng;
 import com.google.android.gms.maps.model.MarkerOptions;
 import com.xavier.spikedroid.R;
 
+import org.honorato.multistatetogglebutton.MultiStateToggleButton;
+import org.honorato.multistatetogglebutton.ToggleButton;
+
+import java.util.ArrayList;
+import java.util.List;
+
 public class GoogleMapsActivity extends FragmentActivity implements OnMapReadyCallback {
 
     private GoogleMap map;
@@ -23,6 +29,8 @@ public class GoogleMapsActivity extends FragmentActivity implements OnMapReadyCa
         SupportMapFragment mapFragment = (SupportMapFragment) getSupportFragmentManager()
                 .findFragmentById(R.id.map);
         mapFragment.getMapAsync(this);
+
+        initToggleButtons();
     }
 
     @Override
@@ -33,5 +41,31 @@ public class GoogleMapsActivity extends FragmentActivity implements OnMapReadyCa
         LatLng sydney = new LatLng(-34, 151);
         map.addMarker(new MarkerOptions().position(sydney).title("Marker in Sydney"));
         map.moveCamera(CameraUpdateFactory.newLatLng(sydney));
+    }
+
+    private void initToggleButtons() {
+        MultiStateToggleButton button = (MultiStateToggleButton) findViewById(R.id.mstb_multi_id);
+
+        boolean[] states = {true, false, false};
+        button.setStates(states);
+
+        button.enableMultipleChoice(false);
+        button.setOnValueChangedListener(new ToggleButton.OnValueChangedListener() {
+            @Override
+            public void onValueChanged(int position) {
+                switch(position){
+                    case 0:
+                        map.setMapType(GoogleMap.MAP_TYPE_NORMAL);
+                        break;
+                    case 1:
+                        map.setMapType(GoogleMap.MAP_TYPE_HYBRID);
+                        break;
+                    case 2:
+                        map.setMapType(GoogleMap.MAP_TYPE_SATELLITE);
+                        break;
+                }
+
+            }
+        });
     }
 }
